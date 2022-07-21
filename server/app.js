0c291417161bdb22b196709a91c00fea6e3379c8
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-21 18:12:02 +0300

    5

--- server/app.js
+++ server/app.js
@@ -25,16 +25,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-if (process.env.NODE_ENV === "development") {
+
+
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.resolve("..", "client", "build")));
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve("..", "client", "build", "index.html"))
+    });
+}else if(process.env.NODE_ENV === "development"){
     var corsOptions = {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3000/",
         optionsSuccessStatus: 200,
     };
     app.use(cors(corsOptions));
 }
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter);
 
 module.exports = app;
